b79e236c2bee148761c9e02a6488cc430c6ecfc1
joao <joaovct92@gmail.com>
2020-05-03 19:20:10 -0700

    updated geom_timeline test

--- tests/testthat/test-data-cleaning-test.R
+++ tests/testthat/test-data-cleaning-test.R
@@ -13,7 +13,7 @@ test_that("eq_map works", {
 })
 
 test_that("geom_timeline works", {
-  expect_is(ggplot(eq_data_plot, aes(x = DATE, y = COUNTRY, min_date = as.Date("1900-01-01"), max_date = as.Date("2000-01-01"), size = EQ_PRIMARY, color = TOTAL_DEATHS)) +
+  expect_is(ggplot(eq_location_clean(eq_clean_data(read.delim(system.file("extdata", "signif.txt", package = "msdrCourseraCapstonePackage")))), aes(x = DATE, y = COUNTRY, min_date = as.Date("1900-01-01"), max_date = as.Date("2000-01-01"), size = EQ_PRIMARY, color = TOTAL_DEATHS)) +
               geom_timeline(alpha = 0.3) +
               geom_timeline_label(aes(n_max = 3, label = LOCATION_NAME)) +
               theme_eq, "gg")
